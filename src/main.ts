ce825c007b50823a16ee9ba40b1dd353d2dc6fdc
Leonardo Redin <redin@hey.com>
2023-06-07 15:34:11 -0300

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Bágdex API')
+    .setDescription('API simples para Bágdex')
+    .setVersion('1.0')
+    .addTag('bagdex')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
