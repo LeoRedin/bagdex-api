99fa1eef974938eacf317731eefcf2f98358048a
Leonardo Redin <redin@hey.com>
2023-08-04 15:28:40 -0300

    added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { EnvironmentConfigModule } from './infrastructure/config/environment.config/environment.config.module';
 import { LoggerModule } from './infrastructure/logger/logger.module';
 import { ExceptionsModule } from './infrastructure/exceptions/exceptions.module';
+import { TypeormModule } from './infrastructure/config/typeorm/typeorm.module';
 
 @Module({
-  imports: [EnvironmentConfigModule, LoggerModule, ExceptionsModule],
+  imports: [EnvironmentConfigModule, LoggerModule, ExceptionsModule, TypeormModule],
   controllers: [],
   providers: [],
 })
